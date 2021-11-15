f806da1853391c38f41475a4bf5aaaf6975e8052
dloyolar <dieegolr@gmail.com>
2021-11-15 16:32:19 -0300

    feat: add timer to log out 🕛

--- script.js
+++ script.js
@@ -178,13 +178,39 @@ const updateUI = acc => {
   calcDisplaySummary(acc);
 };
 
+const startLogOutTimer = function () {
+  const tick = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+    // In each call, print the remaining time to UI
+    labelTimer.textContent = `${min}:${sec}`;
+
+    // When 0 seconds, stop timer and log out user
+    if (time === 0) {
+      clearInterval(timer);
+      labelWelcome.textContent = 'Log in to get started';
+      containerApp.style.opacity = 0;
+    }
+
+    // Decrease 1s
+    time--;
+  };
+  // Set time to 5 minutes
+  let time = 120;
+
+  // Call the timer every second
+  tick();
+  const timer = setInterval(tick, 1000);
+  return timer;
+};
+
 // Events handler
-let currentAccount;
+let currentAccount, timer;
 
 // FAKE ALWAYS LOGGED IN
-currentAccount = account1;
-updateUI(currentAccount);
-containerApp.style.opacity = 100;
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
@@ -232,6 +258,10 @@ btnLogin.addEventListener('click', function (e) {
     inputTransferAmount.value = inputTransferTo.value = '';
     inputLoginPin.blur();
 
+    // Timer
+    if (timer) clearInterval(timer);
+    timer = startLogOutTimer();
+
     updateUI(currentAccount);
   } else {
     alert('Oh! wrong password or user doesnt exists!');
@@ -268,6 +298,10 @@ btnTransfer.addEventListener('click', function (e) {
     );
     // Update UI
     updateUI(currentAccount);
+
+    // Reset timer
+    clearInterval(timer);
+    timer = startLogOutTimer();
   }
   inputTransferAmount.value = inputTransferTo.value = '';
 });
@@ -292,6 +326,9 @@ btnLoan.addEventListener('click', function (e) {
           currentAccount.currency
         )} `
       );
+      // Reset timer
+      clearInterval(timer);
+      timer = startLogOutTimer();
     }, 1000);
 
     inputLoanAmount.value = '';
